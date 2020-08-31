0d66e8532c896dee9f5315776e6563cdb4e630ff
SeanChao <seanchao0804+gh2@gmail.com>
2020-08-31 15:23:12 +0800

    fix

--- clear.sh
+++ clear.sh
@@ -3,5 +3,7 @@ sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
 sudo iptables -t nat -F
 sudo iptables -t mangle -F
+sudo iptables -t raw -F
+sudo iptables -t security -F
 sudo iptables -F
 sudo iptables -X
